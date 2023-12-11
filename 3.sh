47d8d340fa81dd14db1fab2f21878018722ba999
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-12-11 21:57:00 +0100

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -32,7 +32,7 @@ rm /etc/nginx/conf.d/synapse.conf
 sudo cp synapse.conf /etc/nginx/conf.d/synapse.conf
 sudo nginx -t
 sudo systemctl restart nginx
-sudo apt install coturn
+sudo apt install coturn -y
 sudo ufw allow 3478
 sudo ufw allow 5349
 sudo ufw allow 49152:65535/udp
