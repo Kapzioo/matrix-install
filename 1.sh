830f51ba22e9058d10da82bedc33d324da165fc1
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-12-11 21:48:41 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,18 +1,16 @@
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo ufw allow 8448
 sudo ufw allow http
 sudo ufw allow https
 sudo wget -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/matrix-org.list
 sudo apt update
-sudo apt install matrix-synapse-py3
-sudo systemctl status matrix-synapse
+sudo apt install matrix-synapse-py3 -y
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /usr/share/keyrings/postgresql-key.gpg >/dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/postgresql-key.gpg arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo apt update
-sudo apt install postgresql postgresql-contrib
-sudo apt install postgresql-14 postgresql-contrib-14
-sudo systemctl status postgresql
+sudo apt install postgresql postgresql-contrib -y
+sudo apt install postgresql-14 postgresql-contrib-14 -y
 sudo -su postgres
 createuser --pwprompt synapse
 createdb --encoding=UTF8 --locale=C --template=template0 --owner=synapse synapsedb
